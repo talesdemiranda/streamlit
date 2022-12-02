e20086102ce7b584ffa4e1ba7dc9ec16c2aeee8e
talesdemiranda <tales.miranda@gmail.com>
2022-12-02 20:19:19 -0300

    autenticação

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import seguranca.seguranca as seguranca
 
 st.set_page_config("Projeto Stremlit Tales", layout="wide")
 
-menu_esquerdo              = ["Cadastro", "Adesão", "Arrecadação", "Instituto", "Configuração"]
-menu_esquerdo_icone        = ['people-fill', 'card-list', 'cash-coin', 'bounding-box', 'gear']
+if seguranca.check_password():
 
-with st.sidebar:
-   
-   #image = Image.open(local_image)
-   #st.image(image, width=30, )
+	menu_esquerdo              = ["Cadastro", "Adesão", "Arrecadação", "Instituto", "Configuração"]
+	menu_esquerdo_icone        = ['people-fill', 'card-list', 'cash-coin', 'bounding-box', 'gear']
 
-   selected_left_menu = option_menu("Tales",
-                                    menu_esquerdo, 
-                                    icons=menu_esquerdo_icone,
-                                    menu_icon="eyeglasses",
-                                    default_index=0
-                        )
+	with st.sidebar:
+		
+		#image = Image.open(local_image)
+		#st.image(image, width=30, )
+
+		selected_left_menu = option_menu("Tales",
+													menu_esquerdo, 
+													icons=menu_esquerdo_icone,
+													menu_icon="eyeglasses",
+													default_index=0
+									)
