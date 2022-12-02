8c0fc1b363d33051fe876f1368d89279dc2c70ed
talesdemiranda <tales.miranda@gmail.com>
2022-12-02 19:58:24 -0300

    titulo da página

---
+++ commit.bat
@@ -0,0 +1,6 @@
+rem git remote add origin https://github.com/talesdemiranda/streamlit.git
+rem git branch -M main
+rem git push -u origin main
+
+git add *
+git commit -m %1
---
+++ main.py
@@ -0,0 +1,4 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+st.set_page_config("Projeto Stremlit Tales", layout="wide")
