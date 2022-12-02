06a6cd686aaa55ae3f0579b002c4b5d20440d763
talesdemiranda <tales.miranda@gmail.com>
2022-12-02 20:03:56 -0300

    requirements

--- commit.bat
+++ commit.bat
@@ -1,6 +1,6 @@
-rem git remote add origin https://github.com/talesdemiranda/streamlit.git
-rem git branch -M main
-rem git push -u origin main
-
 git add *
 git commit -m %1
+
+git remote add origin https://github.com/talesdemiranda/streamlit.git
+git branch -M main
+git push -u origin main
